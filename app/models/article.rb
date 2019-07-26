148073f64b523d1dbbde707bfbdf680f7015c195
Ludovico Pinzari <ludovico.pinzari@gmail.com>
2019-07-27 08:35:12 +1000

    Add: create, flash, error msgs and show

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id          :bigint           not null, primary key
+#  title       :text
+#  description :text
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
