7f3ca0db56991d621a1291b7c35a7ef43ee0360c
Ludovico Pinzari <ludovico.pinzari@gmail.com>
2019-07-29 21:08:19 +1000

    Fix bug sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
   # before_action :check_for_login, :only => [:edit, :update]
-  before_action :check_for_login, except: [:index, :show, :edit, :update]
+  before_action :check_for_login, except: [:index, :show, :edit, :update, :new, :create]
   before_action :require_same_user, only: [:edit, :update, :destroy]
   before_action :require_admin, only: [:destroy]
 
