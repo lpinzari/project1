444f2bf9b549598a561f25f5ddf89090d0d9c204
Ludovico Pinzari <ludovico.pinzari@gmail.com>
2019-07-30 10:44:30 +1000

    Add: category create and admin check

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,40 @@
+class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    # @category = Category.create :name => "History"
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to categories_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:title,:name)
+  end
+
+  def require_admin
+    if @current_user.present? and !@current_user.admin?
+      flash[:danger] = "Only admin users can perform this operation"
+      redirect_to categories_path
+    end
+  end
+
+end
