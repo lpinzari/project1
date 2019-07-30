aa2e3012c46eefbde015d7dcb15216c2e73804d0
Ludovico Pinzari <ludovico.pinzari@gmail.com>
2019-07-30 11:55:35 +1000

    Fix bug creation article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -49,7 +49,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title,:description)
+    params.require(:article).permit(:title,:description, :id, :name, :category_ids => [])
   end
 
   def require_same_user
