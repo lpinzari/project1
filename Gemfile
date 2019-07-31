cfab576a89700cb999b5381546493d5fbcbe5563
Ludovico Pinzari <ludovico.pinzari@gmail.com>
2019-07-31 14:08:03 +1000

    add image gem and form article

--- Gemfile
+++ Gemfile
@@ -51,3 +51,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'rails_12factor', group: :production
 gem "jquery-rails", "~> 4.3"
 gem 'bootstrap-sass', '~> 3.3.7'
+
+gem 'cloudinary'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
+    if params[:file].present?
+      # Then call Cloudinary's upload method, passing in the file in params
+      req = Cloudinary::Uploader.upload(params[:file])
+      # Using the public_id allows us to use Cloudinary's powerful image
+      # transformation methods.
+      @user.image = req["public_id"]
+      @user.save
+    end
+
     if @user.save
       session[:user_id] = @user.id
       flash[:success] = "Welcome to the Agora Blog #{@user.username}"
@@ -30,6 +39,14 @@ class UsersController < ApplicationController
   end
 
   def update
+
+    if params[:file].present?
+      req = Cloudinary::Uploader.upload(params[:file])
+      @user.image = req["public_id"]
+    end
+      @user.update user_params
+      @user.save
+
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
